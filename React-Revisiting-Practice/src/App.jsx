cb4ebc28188c9d250b2dd6df0ab4e3f5385352e1
SaulWeaponz <saulweaponz@gmail.com>
2025-09-10 21:56:42 +0300

    Add styling and adding images in React

--- React-Revisiting-Practice/src/App.jsx
+++ React-Revisiting-Practice/src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Form from "./form";
+import image from "./assets/Saul.jpg"
+import './App.css';
 function App(){
   let Myname = "Sabunyo Saul"
   return (
     
     <div>
       <h1>Hello, React!</h1>
-      <p>This is my journey into the React World.</p>
+      //inline styling method of css
+      {/* <h1 style={{color:"orangered", textAlign:"center", fontFamily:"'Segoe UI', Tahoma, Geneva, Verdana, sans-serif", marginTop:"20px", textShadow:"2px 2px 4px #aaa", padding:"20px"}}>Hello, React!</h1> */}
+      <p style={{textDecoration:"underline"}}>This is my journey into the React World.</p>
       <p>My Name is {Myname}</p>
       <p>I am enjoying learning React!</p>
       <Form />
+      <img src={image} alt="Saul" />
     </div>
   );
 }
